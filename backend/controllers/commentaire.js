3772335a61fc613ad71e9b687edfe9c8ad644766
meidhi <meidhi14680@gmail.com>
2023-05-05 14:36:33 +0200

    Ajout et Check de createCommentaire / OK

--- backend/controllers/commentaire.js
+++ backend/controllers/commentaire.js
@@ -6,3 +6,16 @@ exports.getAllCommentaires = (req, res, next) => {
     .then((commentaires) => res.status(200).json(commentaires))
     .catch((error) => res.status(400).json({ error }));
 };
+
+// --- Ajouter un commentaire dans le livre d'or ---
+exports.createCommentaire = (req, res, next) => {
+  const commentaire = new Commentaire({
+    prenom: req.body.prenom,
+    description: req.body.description,
+  });
+
+  commentaire
+    .save()
+    .then(() => res.status(201).json({ message: 'Commentaire Créé !' }))
+    .catch((error) => res.status(400).json(error));
+};
--- backend/routes/commentaire.js
+++ backend/routes/commentaire.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const commentaireCtrl = require('../controllers/commentaire');
 
+// --- Récuperer tous les commentaires ---
 router.get('/', commentaireCtrl.getAllCommentaires);
+
+// --- Ajouter un commentaire dans le livre d'or ---
+router.post('/', commentaireCtrl.createCommentaire);
 
 module.exports = router;
